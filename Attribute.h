626e2a0bb85a948ca6a8e8ed33af29e5e0bbec90
MOREL Pierre-Jean <pimorel@enseirb-matmeca.fr>
2021-11-17 16:37:41 +0100

    Format file

--- Attribute.h
+++ Attribute.h
@@ -13,24 +13,22 @@
 
 typedef enum
 {
-  INT,
-  FLOAT
+    INT,
+    FLOAT
 } type;
 
 struct ATTRIBUTE
 {
-  char * name;
-  int int_val;           // utilise' pour NUM et uniquement pour NUM
-  type type_val;
-  
-  /* les autres attributs dont vous pourriez avoir besoin sont déclarés ici */
-  
+    char *name;
+    int int_val; // utilise' pour NUM et uniquement pour NUM
+    type type_val;
+
+    /* les autres attributs dont vous pourriez avoir besoin sont déclarés ici */
 };
 
-typedef struct ATTRIBUTE * attribute;
+typedef struct ATTRIBUTE *attribute;
 
 attribute new_attribute();
 /* returns the pointeur to a newly allocated (but uninitialized) attribute value structure */
 
 #endif
-
